7ce8fc45890101c030da12b05015c90f9da0e761
Deyvid Dushkov <dushkovd@gmail.com>
2025-08-01 17:46:22 +0300

    Add favicon

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -14,6 +14,32 @@ declare global {
   }
 }
 
+// Meta Pixel initialization
+const initMetaPixel = () => {
+  if (typeof window === 'undefined') return;
+  if ((window as any).fbq) return;
+  
+  (function(f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
+    if (f.fbq) return;
+    n = f.fbq = function() {
+      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+  })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js', undefined, undefined, undefined);
+  
+  (window as any).fbq('init', '1287939146186577');
+  (window as any).fbq('track', 'PageView');
+};
+
 // Track initiate checkout event
 const trackInitiateCheckout = (value?: number, currency: string = 'USD', contentIds?: string[]) => {
   if (typeof window !== 'undefined' && (window as any).fbq) {
@@ -55,8 +81,9 @@ const CheckoutPage: React.FC = () => {
     }
   }, [cartGames.length, navigate, t, isAuthenticated]);
 
-  // Track initiate checkout event when user reaches checkout page
+  // Initialize Meta Pixel and track initiate checkout event when user reaches checkout page
   useEffect(() => {
+    initMetaPixel();
     if (cartGames.length > 0) {
       const contentIds = cartGames.map(game => game.id.toString());
       trackInitiateCheckout(totalPrice, 'USD', contentIds);
